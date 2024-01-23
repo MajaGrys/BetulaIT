536a6809dd466edb70bb7e80dbd36d169c7c5022
MajaGrys <majagrys@wp.pl>
2024-01-23 21:59:04 +0100

    Added description

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,15 +1,14 @@
 <script setup>
-import building from '../images/betula.webp';
+import building from '../images/betulait.webp';
 </script>
 
 <template>
   <section id="o-nas">
     <h2 data-aos="zoom-in-up">O nas</h2>
     <div class="section-content">
-      <img :src=building alt="siedziba firmy BetulaIT" data-aos="zoom-in-up" />
-      <p data-aos="zoom-in-up"><span id="logo">BetulaIT</span> lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque illo quia mollitia officia laudantium aliquam fugiat illum fuga eligendi quam, consectetur magni quaerat tempora quibusdam harum? Molestiae inventore ut error reiciendis, aspernatur commodi exercitationem! Vero, delectus a porro distinctio non dolorem dolorum quam id fugiat, blanditiis officia magni explicabo eius quaerat aut provident natus exercitationem?<br /><br />
-      Ut fuga soluta sunt quis sequi, iure distinctio, dignissimos exercitationem, deleniti fugiat quas! Non cum temporibus blanditiis officia deleniti! Qui ipsum suscipit, quisquam natus accusantium aliquam, accusamus earum dignissimos modi nostrum et illum soluta maxime commodi rem odio voluptatibus optio adipisci quis. Deleniti, iure illo!<br /><br />
-      Quaerat maxime, atque magni eum soluta temporibus id nobis est, labore dolorum vel voluptatum sunt ad esse!</p>
+      <img :src=building alt="siedziba firmy Betula IT" data-aos="zoom-in-up" />
+      <p data-aos="zoom-in-up"><span id="about-logo">Betula IT</span> oferuje profesjonalne usługi informatyczne dla firm w Kwidzynie i okolicach. Doświadczony zespół ekspertów zapewnia klientom sprawną i skuteczną pomoc w rozwiązywaniu problemów, a także kompleksowe wsparcie techniczne. Odpowiadamy za prawidłowe funkcjonowanie systemów informatycznych oraz wdrażanie rozwiązań dopasowanych do potrzeb i wymagań klientów.<br /><br />
+      Zapraszamy do skorzystania z naszych usług!</p>
     </div>
   </section>
 </template>
@@ -25,8 +24,7 @@ import building from '../images/betula.webp';
   padding-bottom: 40px;
 }
 
-#logo {
-  font-size: 1.2rem;
+#about-logo {
   color: var(--orange);
   font-weight: 600;
 }
@@ -36,8 +34,8 @@ img {
   height: 80vw;
   max-width: 400px;
   max-height: 400px;
-  /* box-shadow: 0px 0px 5px var(--black); */
-  /* border: 1px solid var(--purple); */
+  box-shadow: 0px 0px 5px #30287b51;
+  /* border: 1px solid #30287b4d; */
   /* border-radius: 20px; */
 }
 
